df13cbaf34658e71cf3a1b14f3a092edb141f52c
sinchanatc <92010565+sinchanatc@users.noreply.github.com>
2023-03-27 21:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
+}
 
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+
+/** 
+ * add ul list in HTML
+ **/
+document.querySelector('#bands').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('')
